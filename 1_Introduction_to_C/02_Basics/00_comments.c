a3c2c12e8e5f0148780f6e701d5f1b797c6a2822
Mustafa <mustafakatipoglu98@hotmail.com>
2020-07-28 03:41:23 +0300

    Add Comment and sizeof Operator Examples

---
+++ 1_Introduction_to_C/02_Basics/00_comments.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+{   
+
+	// This is a one line comment
+    printf("Hello World");
+
+    /*
+	 This is a multi line comment
+	 All the comments will be ignored by the compiler.
+    */
+
+    /* We can also use comments multi line comment in one line */
+
+    printf("Something");  // Lets print out "Something" to the screen.
+
+    return 0;
+}
+
---
+++ 1_Introduction_to_C/02_Basics/01_sizeof_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   
+    
+    printf("%zu\n", sizeof(45));
+    
+    printf("%zu\n", sizeof(45.5));
+
+    return 0;
+}
+
+
