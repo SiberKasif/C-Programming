df5678f190d74a64792c2f1c543f180e89a68615
Mustafa <mustafakatipoglu98@hotmail.com>
2020-07-29 05:19:36 +0300

    Add Double Division - Implicit Conversion examples

---
+++ 1_Introduction_to_C/02_Basics/04_double_type/06_division.c
@@ -0,0 +1,13 @@
+#include <stdio.h>                 
+
+
+int main()
+{   
+
+	// Do you see the implicit conversion?
+	double x = 5/2;
+
+	printf("%f", x);				// why prints 2.0 ? 
+
+    return 0;                      
+}
---
+++ 1_Introduction_to_C/02_Basics/04_double_type/06_division_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>                 
+
+
+int main()
+{   
+
+	double x = 5.0/2;
+	printf("%f", x);
+
+	double y = 5 / 2.0;
+	printf("\n%f", y); 
+
+    return 0;                      
+}
